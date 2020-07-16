6a639401e5d4ea986984852abd426f8557fbbdd2
Vishal Dhanjani <vdhanjani63@gmail.com>
2020-07-16 12:16:26 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,17 +1,17 @@
 import { Meteor } from 'meteor/meteor';
-Images = new Mongo.Collection("images");
-console.log(Images.find().count());
-Meteor.startup(() => {
-	if(Images.find().count()==0){
-		for(var i=1; i<=10; i++){
-			Images.insert(
-				{
-					img_src:"img"+i+".jpg",
-					alt:"img"+i
-				}
-			);//end of insert			
-		}//end of for loop
 
-	}//end of if statement
-console.log(Images.find().count());// code to run on server at startup
+Images=new Mongo.Collection("Images");
+
+Meteor.startup(() => {
+if(Images.find().count()==0){
+	for(var i=1; i<=7; i++){
+		if(i==1 || i==6){
+		 Images.insert({img_src:"img_"+i+".jpg", data:" "});
+		}
+		else{
+		 Images.insert({img_src:"img_"+i+".png", data:" "});	
+		}
+	}
+	console.log(Images.find().count());
+}// code to run on server at startup
 });
